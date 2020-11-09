18ea01d0c7dba3c5076886e15158accc50769189
alejo <anmdev@protonmail.com>
2020-11-08 23:36:35 -0300

    -

--- API Clima.py
+++ API Clima.py
@@ -5,18 +5,19 @@ ciudad = "uruguay"
 
 url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + ciudad + '&appid=e388302d7bbfeb525550c747d087ad8f').read()
 
-# JSON A DICCIONARIO
-data_dic = json.loads(url)
-
+# JSON
+data_json = json.loads(url)
 #print(data_dic)
 
-data = {
-        "CODIGO PAIS": str(data_dic['sys']['country']),
-        "LONGITUD": str(data_dic['coord']['lon']),
-        "LATITUD": str(data_dic['coord']['lat']),
-        "TEMPERATURA": str(data_dic['main']['temp']) + 'k',
-        "PRESION": str(data_dic['main']['pressure']),
-        "HUMEDAD": str(data_dic['main']['humidity']),
+# DICCIONARIO
+data_dic = {
+        "CODIGO PAIS": str(data_json['sys']['country']),
+        "LONGITUD": str(data_json['coord']['lon']),
+        "LATITUD": str(data_json['coord']['lat']),
+        "TEMPERATURA": str(data_json['main']['temp']) + 'k',
+        "PRESION": str(data_json['main']['pressure']),
+        "HUMEDAD": str(data_json['main']['humidity']),
         }
 
-print(data)
+for k,v in data_dic.items():
+        print(k,v)
